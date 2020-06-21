bedcad001a4044db0efce2d18f52f21ae3788212
Alex <snoopcom@bk.ru>
2020-06-21 15:16:31 +0300

    Updates

---
+++ precache-manifest.a26cd27e6675ea446586e09b955f02bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd516c0dfeb68dfca83f89fb7245b38",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "79af2a2528b1fb2630df",
+    "url": "/aviasales/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "58321e5e05b48660b0e0",
+    "url": "/aviasales/static/js/2.2fbdec6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aviasales/static/js/2.2fbdec6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79af2a2528b1fb2630df",
+    "url": "/aviasales/static/js/main.707d4230.chunk.js"
+  },
+  {
+    "revision": "0cc15852d7adf8a9722e",
+    "url": "/aviasales/static/js/runtime-main.b4f11597.js"
+  }
+]);
