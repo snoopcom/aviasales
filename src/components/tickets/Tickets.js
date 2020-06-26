1a73bc65d8df5d17653978b2ea2dd2524aa479ca
Alex <snoopcom@bk.ru>
2020-06-26 23:25:27 +0300

    test

--- src/components/tickets/Tickets.js
+++ src/components/tickets/Tickets.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Sort from '../sort/Sort';
 import TicketsItem from './TicketsItem';
 
-const Tickets = () => {
-  return (
-    <div>
-      <Sort />
-      <TicketsItem />
-    </div>
-  );
-};
+const initState = {};
 
-export default Tickets;
+export default class Tickets extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initState;
+  }
+
+  render() {
+    return (
+      <div>
+        <Sort />
+        <TicketsItem />
+      </div>
+    );
+  }
+}
